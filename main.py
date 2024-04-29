10039fd6230a9d9d10e07e0dc39a3aafadce9520
dbc201 <denizbcakiroglu@gmail.com>
2024-04-28 22:48:04 -0400

    default depth 2 and move pawns at endgame

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from ChessAi import ChessAi
 if __name__ == '__main__':
     ChessGui.init()
     chess_gui = ChessGui()
-    chess_gui.run(None, ChessAi(3))
+    chess_gui.run(None, ChessAi(2))
     ChessGui.quit()
